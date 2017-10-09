3aa16a0a081f38241faa4753c667a4352b16a7fe
pinaki29uk <pinaki29uk@gmail.com>
2017-10-09 16:56:25 +0530

    commit changes in GIT.

---
+++ TestGitProj/src/com/test/MyClass.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my test GIT project");
+	}
+
+}
