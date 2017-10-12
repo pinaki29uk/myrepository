6ee43eb83f8f8d07ec6cbf730d4cf34dac19cf6b
pinaki29uk <pinaki29uk@gmail.com>
2017-10-12 18:08:35 +0530

    3rd changes

--- TestGitProj/src/com/test/MyClass.java
+++ TestGitProj/src/com/test/MyClass.java
@@ -6,7 +6,7 @@ public class MyClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("my test GIT project");
-		System.out.println("modified code cycle 1");
+		System.out.println("modified code cycle 2");
 	}
 
 }
