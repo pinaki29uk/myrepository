5aa5d92f50cfc4ff739523244563893a71227935
pinaki29uk <pinaki29uk@gmail.com>
2017-10-16 12:43:47 +0530

    ----

--- TestGitProj/src/com/test/MyClass.java
+++ TestGitProj/src/com/test/MyClass.java
@@ -6,7 +6,7 @@ public class MyClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("my test GIT project");
-		System.out.println("modified code cycle 3 ");
+		System.out.println("modified code cycle 4 ");
 	}
 
 }
